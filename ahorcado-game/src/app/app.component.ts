8d3f3584636d98096158df70fb848ab707092140
nuevo_63 <ajrahuacso@gmail.com>
2024-06-15 15:48:31 -0500

    Se crea el componente del ahorcado y se importa en el app root general

--- ahorcado-game/src/app/app.component.ts
+++ ahorcado-game/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HangmanFigureComponent } from './hangman-figure/hangman-figure.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HangmanFigureComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'ahorcado-game';
 }
--- ahorcado-game/src/app/hangman-figure/hangman-figure.component.ts
+++ ahorcado-game/src/app/hangman-figure/hangman-figure.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-hangman-figure',
+  selector: 'hangman-figure',
   standalone: true,
   imports: [],
   templateUrl: './hangman-figure.component.html',

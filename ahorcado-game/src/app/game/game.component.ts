1922d733909356a7e1a0c318976588b0c310cdcc
nuevo_63 <ajrahuacso@gmail.com>
2024-06-15 20:53:09 -0500

    Se crean enlaces entre componentes

--- ahorcado-game/src/app/game/game.component.ts
+++ ahorcado-game/src/app/game/game.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { GameBoardComponent } from './game-board/game-board.component';
+import { HangmanFigureComponent } from './hangman-figure/hangman-figure.component';
+import { LetterBarComponent } from './letter-bar/letter-bar.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [GameBoardComponent, HangmanFigureComponent, LetterBarComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })

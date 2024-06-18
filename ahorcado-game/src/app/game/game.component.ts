f43ddb26bdb4d3287cd3a8b1cc8ab45933558bb7
nuevo_63 <ajrahuacso@gmail.com>
2024-06-17 23:19:56 -0500

    Se agregan estilos complementarioss

--- ahorcado-game/src/app/game/game.component.ts
+++ ahorcado-game/src/app/game/game.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { GameBoardComponent } from './game-board/game-board.component';
 import { HangmanFigureComponent } from './hangman-figure/hangman-figure.component';
-import { LetterBarComponent } from './letter-bar/letter-bar.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [GameBoardComponent, HangmanFigureComponent, LetterBarComponent],
+  imports: [GameBoardComponent, HangmanFigureComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
